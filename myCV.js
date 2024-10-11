910d6e8e0748aeb262ca85793484372ec9f44f2d
Jean-Baptiste Lebrun <jb.lebrun@icloud.com>
2024-10-11 17:24:11 +0200

    [DEV]: Javascript code optimisation

--- myCV.js
+++ myCV.js
@@ -1,91 +1,41 @@
 const menuItems = document.querySelectorAll('.pageHeaderMenu .menuItem a');
-const menuCentralPicture = document.querySelector('.pictureContainer .aboutMe img');
-const sectionItems = document.querySelectorAll('section');
+const menuCentralPicture = document.querySelector('.pageHeaderMenu .pictureContainer .aboutMe img');
 
 menuItems.forEach((item) => {
-    item.addEventListener('click', function (event) {
-        event.preventDefault();
-        const menuItemName = event.target.className;
+    item.addEventListener(
+        'click', function (event) {
+            event.preventDefault();
+            const menuItemName = event.target.className;
+            const bodyClassName = document.body.className;
+            if (bodyClassName != menuItemName + 'Theme') {
+                changeTheme(menuItemName);
+            };
+        }
+    );
+});
+
+menuCentralPicture.addEventListener(
+    'click', function (event) {
         const bodyClassName = document.body.className;
-
-        if (menuItemName === "myCV" && bodyClassName !== menuItemName + "SectionTheme") {
-            document.body.className = menuItemName + "SectionTheme";
-
-            sectionItems.forEach((section) => {
-                section.classList.remove('active');
-                if (section.classList.contains(menuItemName + 'Section')){
-                    section.classList.add('active');
-                } 
-            });
-
-        }
-
-        else if (menuItemName === "myTrainings" && bodyClassName !== menuItemName + "SectionTheme") {
-            document.body.className = menuItemName + "SectionTheme";
-
-            sectionItems.forEach((section) => {
-                section.classList.remove('active');
-                if (section.classList.contains(menuItemName + 'Section')){
-                    section.classList.add('active');
-                } 
-            });
-
-        }
-
-        else if (menuItemName === "aboutMe" && bodyClassName !== menuItemName + "SectionTheme") {
-            document.body.className = menuItemName + "SectionTheme";
-
-            sectionItems.forEach((section) => {
-                section.classList.remove('active');
-                if (section.classList.contains(menuItemName + 'Section')){
-                    section.classList.add('active');
-                } 
-            });
-
-        }
-
-        else if (menuItemName === "myExperience" && bodyClassName !== menuItemName + "SectionTheme") {
-            document.body.className = menuItemName + "SectionTheme";
-
-            sectionItems.forEach((section) => {
-                section.classList.remove('active');
-                if (section.classList.contains(menuItemName + 'Section')){
-                    section.classList.add('active');
-                } 
-            });
-
-        }
-
-        else if (menuItemName === "mySkills" && bodyClassName !== menuItemName + "SectionTheme") {
-            document.body.className = menuItemName + "SectionTheme";
-
-            sectionItems.forEach((section) => {
-                section.classList.remove('active');
-                if (section.classList.contains(menuItemName + 'Section')){
-                    section.classList.add('active');
-                } 
-            });
-
-        }
-    });
-});
-
-menuCentralPicture.addEventListener('click', function (event) {
-    event.preventDefault();
-    handleMenuClick('aboutMe');
-});
-
-function handleMenuClick(menuItemName) {
-    const bodyClassName = document.body.className;
-
-    if (bodyClassName !== menuItemName + "SectionTheme") {
-        document.body.className = menuItemName + "SectionTheme";
-
-        sectionItems.forEach((section) => {
-            section.classList.remove('active');
-            if (section.classList.contains(menuItemName + 'Section')) {
-                section.classList.add('active');
-            }
-        });
+        event.preventDefault();
+        if (bodyClassName != 'aboutMeTheme') {
+            changeTheme('aboutMe');
+        };
     }
+);
+
+function changeTheme(menuItemName) {
+    const toggleItems = document.querySelectorAll('.active, .inactive')
+    document.body.className = menuItemName + 'Theme';
+    toggleItems.forEach(
+        (item) => {
+            if (!item.classList.contains(menuItemName + 'Section') && item.classList.contains('active')) {
+                item.classList.remove('active');
+                item.classList.add('inactive');
+            } else if (item.classList.contains(menuItemName + 'Section') && item.classList.contains('inactive')) {
+                item.classList.remove('inactive');
+                item.classList.add('active');
+            }
+        }
+    );
 }
